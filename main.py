36d98e54e25bb03b823e3aea2e03d85be8f3ccc3
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-04-26 11:16:23 +0600

    Working Fine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 
+ID = 836044973778141254
+
 
 def read_token():
     with open("K:\\Project\\Python\\MiracalDiscordBot\\token", "r") as f:
@@ -12,10 +14,21 @@ token = read_token()
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    if message.content.find("hello") != -1:
-        await message.channel.send("hi")
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            await channel.send_message(f"""Welcome to the server {member.mention}""")
 
+@client.event
+async def on_message(message):
+    id = client.get_guild(ID)
+    channels = ["commands"]
+
+    if str(message.channel) in channels: # Check if in correct channel
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi")
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
 
 
 client.run(token)
